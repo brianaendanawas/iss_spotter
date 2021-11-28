b477a243832f3bc32ddf89d277cac35c4b8fa522
Briana Endanawas <brianamay48@gmail.com>
2021-11-28 01:56:10 -0500

    calling fetchISSFlyOverTimes

---
+++ index.js
@@ -0,0 +1,30 @@
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('142.51.208.104', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+fetchISSFlyOverTimes({ latitude: 44.0229, longitude: -79.8012 }, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});

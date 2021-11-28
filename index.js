f208671aff2a99ad9a9abb9d8ebd009e33ff04e4
Briana Endanawas <brianamay48@gmail.com>
2021-11-28 07:05:36 -0500

    calling fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
-
-fetchCoordsByIP('142.51.208.104', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned coordinates:' , coordinates);
-});
-
-fetchISSFlyOverTimes({ latitude: 44.0229, longitude: -79.8012 }, (error, passTimes) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned flyover times:' , passTimes);
-});
+  printPassTimes(passTimes);
+})
